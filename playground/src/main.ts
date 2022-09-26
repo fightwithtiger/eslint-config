8ff69fa32e1eff3e9eb027750615a265c6b9e201
dengtuo <2284925818@qq.com>
2022-09-26 18:15:13 +0800

    feat:  update brace-style

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,7 +1,8 @@
-export const a = "a"
+export const a = 'a'
 
-export const list = [1, 2,3]
+export const list = [1, 2, 3]
 if (a === 'a') {
+  // eslint-disable-next-line no-console
   console.log('log')
 } else {
   console.warn('warn')
